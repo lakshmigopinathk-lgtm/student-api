1e9597645745e1830f91e22740a8399a7b464102
lakshmigopinathk-lgtm <lakshmi.gopinath.k@gmail.com>
2026-05-25 15:03:11 +0530

    Updated API

--- app.py
+++ app.py
@@ -18,12 +18,21 @@ class Student(db.Model):
     class_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
 
+
 # Create POST API
 @app.route('/student', methods=['POST'])
 def add_student():
 
     # Receive JSON data from Postman
     data = request.get_json()
+    
+    # Check duplicate email
+    existing_student = Student.query.filter_by(email=data['email']).first()
+
+    if existing_student:
+        return {
+            "message": "already registered"
+        }
 
     # Create student object
     student = Student(
@@ -31,6 +40,19 @@ def add_student():
         class_name=data['class_name'],
         email=data['email']
     )
+    if student.id:
+        existing_student = Student.query.get(student.id)
+
+        if not existing_student:
+            return jsonify({
+                "message": "Student not found"
+            }), 404
+        existing_student.first_name=student.first_name
+        existing_student.class_name=student.class_name
+        existing_student.email=student.email
+    
+        student=existing_student
+
 
     # Save into database
     db.session.add(student)
@@ -53,6 +75,7 @@ def get_student(id):
         return jsonify({
             "message": "Student not found"
         }), 404
+    
 
     # Return student details
     return jsonify({
@@ -79,6 +102,76 @@ def get_all_students():
         })
 
     # Return list
+    return jsonify(student_list)
+@app.route('/students/filter', methods=['GET'])
+def filter_students():
+
+    # Get request parameters
+    class_name = request.args.get('class_name')
+    first_name = request.args.get('first_name')
+
+    # Start query
+    query = Student.query
+
+    # Apply filters
+    if class_name:
+        query = query.filter_by(class_name=class_name)
+
+    if first_name:
+        query = query.filter_by(first_name=first_name)
+
+    # Get filtered students
+    students = query.all()
+
+    # Convert to list
+    student_list = []
+
+    for student in students:
+        student_list.append({
+            "id": student.id,
+            "first_name": student.first_name,
+            "class_name": student.class_name,
+            "email": student.email
+        })
+
+    return jsonify(student_list)
+#deleting
+@app.route('/delete/<int:id>', methods=['DELETE'])
+def delete_student(id):
+
+    student = Student.query.get(id)
+
+    if student:
+        db.session.delete(student)
+        db.session.commit()
+
+        return {
+            "message": "Student deleted successfully"
+        }
+
+    return {
+        "message": "Student not found"
+    }
+#finding student based on their names 
+@app.route('/search', methods=['GET'])
+def search_student():
+
+    name = request.args.get('name')
+
+    students = Student.query.filter(
+        Student.first_name.like(f'{name}%')
+    ).all()
+
+    student_list = []
+
+    for student in students:
+        student_list.append({
+            "id": student.id,
+            "first_name": student.first_name,
+            "class_name": student.class_name,
+            "email": student.email
+        })
+
     return jsonify(student_list)
 # Run application
 if __name__ == '__main__':
